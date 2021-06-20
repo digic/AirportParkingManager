c6bc2f47bc458b4f40303411e4ab1612ea207ded
digic <davidms.dev@gmail.com>
2021-06-20 14:26:19 +0100

    Add project files.

---
+++ AirportParkingManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AirportParkingManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
